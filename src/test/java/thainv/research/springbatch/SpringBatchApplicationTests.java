06b62287f63862ad95cf5f352b58d280ce1d64ab
meogau <thainguyen1421999@gmail.com>
2024-08-03 11:20:04 +0700

    init: init project

---
+++ src/test/java/thainv/research/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package thainv.research.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
